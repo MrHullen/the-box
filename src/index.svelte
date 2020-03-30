eead871e8e9a07c5679f4e67f97d10c688049b32
Mr. Hullen <d.hullen@rangiruru.school.nz>
2020-03-30 15:18:41 +1300

    Box contents viewable when unlocked.

--- src/index.svelte
+++ src/index.svelte
@@ -22,9 +22,15 @@
 {#if box.locked}
   <p>The box is locked.</p>
   <button on:click={unlock}>Unlock</button>
+  <!-- do nothing when box is locked. -->
 
 {:else}
   <p>The box is unlocked.</p>
   <button on:click={lock}>Lock</button>
-
+  <ul>
+    {#each box.contents as item}
+      <li>{item}</li>
+    {/each}
+  </ul>
+  
 {/if}
